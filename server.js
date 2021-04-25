c2ee5443691c3ce2ec12ef7eae31121e9f5b3d88
Micheal Ellis <mellis.eriecon@gmail.com>
2021-04-25 00:14:38 -0400

    added get requests

--- server.js
+++ server.js
@@ -7,7 +7,12 @@ const notes = require('./Develop/db/db.json')
 app.get('/api/notes', (req, res) => {
     res.json(notes)
 });
-
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './Develop/public/notes.html'))
+})
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './Develop/public/index.html'))
+})
 
 
 app.listen(PORT, () => {
