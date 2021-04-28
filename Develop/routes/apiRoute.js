48ce3fe6483d54551be5e2be00b8eae23199a923
Micheal Ellis <76069321+MichealJWEllis@users.noreply.github.com>
2021-04-28 14:31:50 -0400

    code clean / added delete / delete not working

--- Develop/routes/apiRoute.js
+++ Develop/routes/apiRoute.js
@@ -5,7 +5,7 @@ module.exports = function(app) {
 
   function writeNotes(note) {
     note = JSON.stringify(note)
-    console.log(note)
+    // console.log(note)
 
     fs.writeFileSync('./Develop/db/db.json', note, function(e){
       if (e) {
@@ -15,22 +15,24 @@ module.exports = function(app) {
   }
 
   app.get('/api/notes', function(req, res) {
-    console.log(notesDb)
+    // console.log(req.body)
     res.json(notesDb)
   })
 
   app.post('/api/notes', function(req, res) {
-    //   if (notesDb.length == 0){
-    //     req.body.id = "0";
-    // } else{
-    //     req.body.id = JSON.stringify(JSON.parse(notesDb[notesDb.length - 1].id) + 1);
-    // }
-
+      // console.log(notesDb.length)
+      if (notesDb.length == 0){
+        req.body.id = "0";
+      } else {
+        req.body.id = notesDb.length.toString()
+      }
     notesDb.push(req.body)
     writeNotes(notesDb)
     res.json(req.body)
   })
-  
-  // writeNotes(notesDb)
+
+  app.delete('/api/notes/:id', function(req, res) {
+    console.log()
+  })
 }
 
